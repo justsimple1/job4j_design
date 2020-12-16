24fa33c3127ba1d5bd9f3c979f6fdd6774be676d
justsimple1 <dla-raboti5@yandex.ru>
2020-12-16 13:17:12 +0300

    6. Перевернуть связанный список[161#423151]

---
+++ src/main/java/ru/job4j/collection/ForwardLink.java
@@ -0,0 +1,69 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ForwardLink<T> implements Iterable<T> {
+    private Node<T> head;
+    private int size = 0;
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        size++;
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public void revert() {
+        int count = size;
+        Node<T> next = head;
+        Node<T> previous = null;
+        while (count != 0) {
+                Node<T> current = next;
+                previous = current.next;
+                next = previous;
+                next.next = current;
+                count--;
+            }
+        head = next;
+        }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+public class ForwardLinkedTest {
+
+    @Test
+    public void whenAddThenIter() {
+        ForwardLink<Integer> linked = new ForwardLink<>();
+        linked.add(1);
+        linked.add(2);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenAddAndRevertThenIter() {
+        ForwardLink<Integer> linked = new ForwardLink<>();
+        linked.add(1);
+        linked.add(2);
+        linked.revert();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+}
