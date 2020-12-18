7e563ef40bb5563c3c1167b6da162dd2f681060a
justsimple1 <dla-raboti5@yandex.ru>
2020-12-18 19:14:58 +0300

    5. Очередь на двух стеках[160#423150]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,42 +1,19 @@
 package ru.job4j.collection;
 
-import java.util.NoSuchElementException;
-
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int size = 0;
 
     public T poll() {
-        if (in.size == out.size) {
-            throw new NoSuchElementException();
-        }
-        boolean revert = false;
-        T temp = null;
-            while (in.size > -1) {
-                if (in.size == 1 && !revert) {
-                    temp = in.pop();
-                } else {
-                    if (in.size == 0) {
-                        revert = true;
-                    }
-                    if (!revert) {
-                        out.push(in.pop());
-                    } else {
-                        if (out.size != 0) {
-                            in.push(out.pop());
-                        } else {
-                            break;
-                        }
-                    }
+            if (out.size() == 0) {
+                while (in.size() > 0) {
+                    out.push(in.pop());
                 }
             }
-        size--;
-        return temp;
+        return out.pop();
     }
 
     public void push(T value) {
         in.push(value);
-        size++;
     }
 }
--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -2,7 +2,8 @@ package ru.job4j.collection;
 
 public class SimpleStack<T> {
     private ForwardLinked<T> linked = new ForwardLinked<T>();
-    int size = 0;
+    private  int size = 0;
+
     public T pop() {
         size--;
         return linked.deleteLast();
@@ -11,5 +12,9 @@ public class SimpleStack<T> {
     public void push(T value) {
         size++;
         linked.add(value);
+    }
+
+    public int size() {
+        return size;
     }
 }
