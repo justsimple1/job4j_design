052104625711f35f3982f54e217bece39a7d9972
justsimple1 <dla-raboti5@yandex.ru>
2020-12-21 17:24:00 +0300

    Init travis & jacoco

--- src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -9,7 +9,7 @@ public class SimpleArrayTest {
 
     @Test
     public void whenAddToArray() {
-        SimpleArray<Integer> numbers = new SimpleArray(5);
+        SimpleArray<Integer> numbers = new ru.job4j.generics.SimpleArray<>(5);
         Iterator<Integer> it = numbers.iterator();
         assertThat(numbers.add(13), is(true));
         assertThat(it.next(), is(13));
@@ -17,7 +17,7 @@ public class SimpleArrayTest {
 
     @Test
     public void whenSetThanTrue() {
-        SimpleArray<Integer> numbers = new SimpleArray(5);
+        SimpleArray<Integer> numbers = new ru.job4j.generics.SimpleArray<>(5);
         Iterator<Integer> it = numbers.iterator();
         numbers.add(13);
         assertThat(numbers.set(0, 15), is(true));
@@ -26,7 +26,7 @@ public class SimpleArrayTest {
 
     @Test
     public void whenRemoveThanTrue() {
-        SimpleArray<String> numbers = new SimpleArray(5);
+        SimpleArray<String> numbers = new ru.job4j.generics.SimpleArray<>(5);
         Iterator<String> it = numbers.iterator();
         numbers.add("first");
         numbers.add("second");
