ee4dcda61235306ccbece7ab275778aa2bca0954
justsimple1 <dla-raboti5@yandex.ru>
2021-03-23 10:13:49 +0300

    5. Валидация параметров запуска.[246865#423195]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -5,15 +5,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".xml")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
-    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+    public static List<Path> search(Path root, String condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -7,15 +7,14 @@ import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SearchFiles implements FileVisitor<Path> {
 
-    private Predicate<Path> condition;
+    private String condition;
     private List<Path> list = new ArrayList<>();
 
-    SearchFiles(Predicate<Path> condition) {
+    SearchFiles(String condition) {
         this.condition = condition;
     }
 
@@ -25,7 +24,7 @@ public class SearchFiles implements FileVisitor<Path> {
     }
 
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (condition.test(file)) {
+        if (file.getFileName().toString().contains(condition)) {
             list.add(file);
         }
         return CONTINUE;
