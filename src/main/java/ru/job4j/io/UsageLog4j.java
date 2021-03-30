455fcc1e28b3444179583d654c09fb2ce8a89005
justsimple1 <dla-raboti5@yandex.ru>
2021-03-30 13:27:52 +0300

    3. Slf4j - вывод переменных.[268852#423190]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,14 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        int age = 24;
+        double weight = 78D;
+        float height = 190F;
+        long hairCount = 23434342432L;
+        boolean male = true;
+        char firstSymbolName = 'N';
+        byte firstValue = 1;
+        short secondValue = 15;
+        LOG.debug("User info secondValue : {}, age : {}, weight : {}, height : {}, hairCount : {}, male : {}, firstSymbolName : {}, firstValue : {}", secondValue, age, weight, height, hairCount, male, firstSymbolName, firstValue);
     }
 }
