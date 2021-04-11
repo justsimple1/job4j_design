489632f7f8ad444efb511eaaf73e6a4a1c8bbd44
justsimple1 <dla-raboti5@yandex.ru>
2021-04-11 16:12:03 +0300

    1. Бот[7921#423219]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,40 +1,37 @@
 package ru.job4j.io;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str = "";
-                    while (!(str = in.readLine()).isEmpty()) {
+                    String str = in.readLine();
+                    String temp = "";
+                    while (!str.isEmpty()) {
+                        System.out.println(str + "yes!!");
                         if (str.contains("Hello")) {
-                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
                             out.write("Hello, dear friend.".getBytes());
                             break;
                         } else if (str.contains("Exit")) {
-                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
                             server.close();
                         } else {
-                            out.write("HTTP/1.1 200 OK\r\n".getBytes());
                             out.write("What ?".getBytes());
                             break;
                         }
-                    }
+                        temp = in.readLine();
+                        if (!temp.isEmpty()) {
+                            str = temp;
+                        }
+                            break;
+                        }
+                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
                 }
             }
-        } catch (Exception e) {
-            LOG.error("Exception in log example", e);
         }
     }
 }
