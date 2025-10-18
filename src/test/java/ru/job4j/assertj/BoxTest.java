0233d0d78b0226923634cc4450d41b42b9c42f95
Панин Никита Николаевич <npanin@gid.ru>
2025-10-18 19:16:53 +0300

    3. Утверждения с примитивными типами [#504883]

---
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BoxTest {
+    @Test
+    void isThisSphere() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Sphere");
+    }
+
+    @Test
+    void isThisTetrahedron() {
+        Box box = new Box(4, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void isThisCube() {
+        Box box = new Box(8, 10);
+        String name = box.whatsThis();
+        assertThat(name).isEqualTo("Cube");
+    }
+
+    @Test
+    void isThisSphereThanAreaExist() {
+        Box box = new Box(0, 10);
+        String name = box.whatsThis();
+        double result = box.getArea();
+        assertThat(result).isNotZero()
+                .isPositive()
+                .isGreaterThan(1);
+    }
+}
--- src/test/java/ru/job4j/iterator/ArrayItTest.java
+++ src/test/java/ru/job4j/iterator/ArrayItTest.java
@@ -7,11 +7,15 @@ class ArrayItTest {
 
     @Test
     void whenMultiCallHasNextThenTrue() {
+        /* Arrange - создание объекта и заполнение его данными */
         ArrayIt iterator = new ArrayIt(
                 new int[] {1, 2, 3}
         );
+        /* Act - выполнение действия и сохранение результата */
         boolean result = iterator.hasNext();
+        /* Assert - оценка соответствия результата ожидаемому исходу */
         assertThat(result).isTrue();
+        /* этапы Act и Assert могут быть размещены в одной строке кода */
         assertThat(iterator.hasNext()).isTrue();
     }
 
