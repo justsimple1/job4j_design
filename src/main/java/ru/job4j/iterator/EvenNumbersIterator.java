77d15f4c5e909620e9bbb0fd273f4fd18fc5176d
Панин Никита Николаевич <npanin@gid.ru>
2025-10-31 23:59:27 +0300

    3. Итератор четных чисел [#150 #27925]

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -16,10 +16,10 @@ public class EvenNumbersIterator implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        while (index < data.length - 1 && data[index] % 2 != 0) {
+        while (index < data.length && data[index] % 2 != 0) {
             index++;
         }
-        return index < data.length && data[index] % 2 == 0;
+        return index < data.length;
     }
 
     @Override
